4b61d593572621c3ec37ea796abab0b4e09f0589
witwiki <Vikram.Udyawer@community.isunet.edu>
2015-02-18 23:02:29 +1300

    prelim changes
    mostly working on .replace() and .append()/.prepend()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,15 @@
-$("#main").append("Vikram Udyawer");
+//var awesomeThoughts = "I am witwiki and I am AWESOME!";
+//console.log(awesomeThoughts);
+
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//$("#main").append(funThoughts);
+
+var name = "Vikram Udyawer";
+var role = "Engineer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);	// want a name to appear first
+$("#header").prepend(formattedName);	// thats why we use .prepend()
+
